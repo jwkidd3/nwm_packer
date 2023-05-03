f85aba09bece43633f1491b2547908a7e05a81ee
jwkidd3 <jkidd@kiddcorp.com>
2023-05-03 10:15:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,22 @@ resource "null_resource" "packer" {
     command = "packer build theimage.json"
   }
 }
+data "aws_ami" "example" {
+  most_recent      = true
+  owners           = ["001613358280"]
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  depends_on=[null_resource.packer]
+}
+
+output "ami"{
+  value=data.aws_ami.example.name
+}
